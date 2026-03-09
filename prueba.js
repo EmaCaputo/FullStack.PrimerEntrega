2610f9f4fb730829133d33c390b1bc593f7bbad5
EmaCaputo <e.caputoblason@gmail.com>
2026-03-09 20:58:48 -0300

    prueba de commit con json de prueba

---
+++ prueba.js
@@ -0,0 +1,16 @@
+console.log("Hola") //visualiuzacion en consola
+
+x = 1 // defino variables
+y = "pepe"
+
+const nombre = "Jorge"
+
+var Http = "http"
+
+function getAll(x,y){
+    z = x + y
+    return z
+}
+
+console.log(getAll(1,4))
+console.log(`Mi nombre es: ${y}`) //estas comillas las escribo con Alt GR + }
